2fbabb202509eecaddfc3de7bb22dc8211bbe46b
Aaron <text>
2022-02-08 16:52:47 +0100

    pruebas de deposito

---
+++ DepositoCombustible/src/test.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class test {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetDepositoNivel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaVacio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaLleno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
